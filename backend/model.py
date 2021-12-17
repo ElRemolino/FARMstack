d947969770b58a255daf592bf58a2a893c017211
Eddie <elremolino91@gmail.com>
2021-12-16 18:46:34 -0800

    made the user schema and userlogin schema

--- backend/model.py
+++ backend/model.py
@@ -1,2 +1,26 @@
+from pydantic import BaseModel, Field, EmailStr
 
+class UserSchema(BaseModel):
+  fullname : str = Field(default = None)
+  email : EmailStr = Field(default = None)
+  password : str = Field(default = None)
+  class Config:
+    the_schema = {
+        "user_demo": {
+          "name" : "Ed",
+          "email" : "elremolino91@gmail.com",
+          "password" : "123"
+        }
+    }
+
+class UserLoginSchema(BaseModel):
+  email : EmailStr = Field(default = None)
+  password : str = Field(default = None)
+  class Config:
+    the_schema = {
+        "user_demo": {
+          "email" : "elremolino91@gmail.com",
+          "password" : "123"
+        }
+    }
 
