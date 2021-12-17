74ced97638e28db312c3444242144b3d4068c44f
Eddie <elremolino91@gmail.com>
2021-12-16 19:50:25 -0800

    user signup route built out

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
+import fastapi
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from model import UserSchema, UserLoginSchema
+from jwt_handler import signJWT
 
 app = FastAPI()
 
@@ -10,3 +13,14 @@ users = []
 def greet():
     return {'hello': 'world'}
 
+# User Signup
+@app.post("user/signup", tags=["user"])
+def user_signup(user: UserSchema = Body(default = None)):
+    users.append(user)
+    return signJWT(user.email)
+
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+        return False
